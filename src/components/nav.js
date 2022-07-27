f638f810240e84a2834b10cb9ce104028906ab34
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2022-07-27 15:46:06 +1200

    mobile menu properly fixed now

--- src/components/nav.js
+++ src/components/nav.js
@@ -97,7 +97,7 @@ ${'' /* min-height: 200vh; */}
 width: 100%;
 display: flex;
 flex-direction: column;
-justify-content: center;
+justify-content: flex-start;
 align-items: center;
 transform: translateY(-3%);
 button:hover {
@@ -216,7 +216,7 @@ max-width: 700px;
     width: 80%;
     padding: 5px 20px;
     :first-of-type {
-        margin-top: 100px !important;
+        margin-top: 50px !important;
     }
     /* margin: 0 40px; */
 }
