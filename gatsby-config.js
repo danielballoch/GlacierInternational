13fb4cd05b9c241a9d405543e3a75207ab58fa58
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2022-08-04 10:27:13 +1200

    add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.glacier.thoughtfulhq.com`
   },
   plugins: ["gatsby-plugin-emotion", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
