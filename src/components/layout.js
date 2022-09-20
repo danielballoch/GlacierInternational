d11558c17760beba78f5b7bc8efd45819b23b172
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2022-09-20 16:01:46 +1200

    update api

--- src/components/layout.js
+++ src/components/layout.js
@@ -59,7 +59,6 @@ const Layout = ({ title, children, pageLocation, hideFooter, invertNav}) => {
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
             <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100&family=Open+Sans&display=swap" rel="stylesheet"/> 
-            {/* <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBHN6Wv8CGtS9AnKBioiTBo0xT1-9BnO8&libraries=places&callback=initMap"/> */}
             <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_PLACES}&libraries=places`} async></script>
 
       </Helmet>
