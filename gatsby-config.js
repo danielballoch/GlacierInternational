94865d9da6b28f90ec30a1bb5b6e0f54f384d3ea
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2022-09-22 11:16:19 +1200

    merge working

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,16 +26,6 @@ module.exports = {
     },
   },
   {
-    resolve: `gatsby-plugin-clarity`,
-    options: {
-      clarity_project_id: "dqtq2m7doy",
-      // Boolean value for enabling clarity while developing
-      // true will enable clarity tracking code on both development and production environments
-      // false will enable clarity tracking code on production environment only
-      enable_on_dev_env: true
-    },
-  },
-  {
     resolve: `gatsby-plugin-google-gtag`,
     options: {
       // You can add multiple tracking ids and a pageview event will be fired for all of them.
