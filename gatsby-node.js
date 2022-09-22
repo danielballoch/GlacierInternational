43ea26310257b166dab084dc9b1e093e66e02a18
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2022-09-22 12:53:47 +1200

    fix

--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-exports.createPages = ({ actions }) => {
-    const { createRedirect } = actions;
-    createRedirect({
-      fromPath: "https://tundra.nz/*", 
-      toPath: "https://www.glacier.nz/:splat", 
-      isPermanent: true, 
-      force: true
-    });
-  };
